85745dd92a303bd7612998583e8605e376eadda5
matviy654 <matviy22.2008@gmail.com>
2024-07-01 20:28:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+
+# expression = input("Введіть арифметичний вираз (наприклад, 23+12): ")
+
+
+# number1_str, operation, number2_str = expression.split()
+
+
+# number1 = int(number1_str)
+# number2 = int(number2_str)
+
+
+# if operation == '+':
+#     result = number1 + number2
+# elif operation == '-':
+#     result = number1 - number2
+# elif operation == '*':
+#     result = number1 * number2
+# elif operation == '/':
+  
+#     if number2 != 0:
+#         result = number1 / number2
+#     else:
+#         print("Помилка: ділення на нуль!")
+#         result = None  
+# else:
+#     print("Непідтримувана операція. Введіть операцію +, -, * або /.")
+
+
+# if result is not None:
+#     print(f"Результат: {result}")
+# import random
+
+
+# random_numbers = [random.randint(-100, 100) for _ in range(20)]  
+
+
+# min_number = min(random_numbers)
+# max_number = max(random_numbers)
+
+
+# count_negative = 0
+# count_positive = 0
+# count_zero = 0
+
+
+# for num in random_numbers:
+#     if num < 0:
+#         count_negative += 1
+#     elif num > 0:
+#         count_positive += 1
+#     else:
+#         count_zero += 1
+
+# print("Список випадкових чисел:", random_numbers)
+# print("Мінімальний елемент:", min_number)
+# print("Максимальний елемент:", max_number)
+# print("Кількість від'ємних елементів:", count_negative)
+# print("Кількість додатніх елементів:", count_positive)
+# print("Кількість нулів:", count_zero)
